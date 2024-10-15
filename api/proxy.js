f922df4d9637fa878e19e327843831ba5cbaed0c
Brendan R <Brendanredinger24@gmail.com>
2024-10-15 14:50:54 -0600

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,16 +1,21 @@
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 
-module.exports = async (req, res) => {
-  const url = 'https://api.countapi.xyz/hit/stonemillwyo.com/1403-windsor-c';
+export default async (req, res) => {
+  const { pageKey } = req.query;
+
+  const namespace = 'stonemillwyo.com'; // Your namespace in CountAPI
+  const API_URL = `https://api.countapi.xyz/hit/${namespace}/${pageKey}`;
 
   try {
-    const response = await fetch(url);
-    const data = await response.json();
+    const apiResponse = await fetch(API_URL);
+    const data = await apiResponse.json();
 
-    // Send the response back to the frontend
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+
     res.status(200).json(data);
   } catch (error) {
-    console.error('Error fetching views:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Failed to fetch view count' });
   }
 };
